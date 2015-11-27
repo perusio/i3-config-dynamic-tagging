28263f213b33b6c93f5fb6d6842260ae83e0e231
António P. P. Almeida <appa@perusio.net>
2015-11-27 11:58:21 +0100

    * Sorting added to workspace list.

--- i3-dtags-moveto.sh
+++ i3-dtags-moveto.sh
@@ -5,4 +5,4 @@ I3MSG=$(command -v i3-msg) || exit 1
 JQ=$(command -v jq) || exit 2
 
 ## Using jq to extract the keys from the JSON array returned by i3-msg -t get_workspaces.
-$I3MSG -t command move workspace $($I3MSG -t get_workspaces | $JQ -M '.[] | .name' | tr -d '"' | dmenu -b -i "$@")
+$I3MSG -t command move workspace $($I3MSG -t get_workspaces | $JQ -M '.[] | .name' | tr -d '"' | sort -u | dmenu -b -i "$@")
